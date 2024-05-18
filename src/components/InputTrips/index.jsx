6e5da531c999c6aa11e184aac7dd0b47f5a1b393
caioteixeira <caio.h.teixeira@gmail.com>
2024-05-17 23:07:41 -0300

    Ajuste na pesquisa de viagem

--- src/components/InputTrips/index.jsx
+++ src/components/InputTrips/index.jsx
@@ -1,9 +1,29 @@
+import { useContext } from 'react';
+import { TripsContext } from '../../context/Trips';
 import './index.css'
 
+import axios from 'axios'
+
 export default function InputTrips() {
+    const { setTripsConsulta } = useContext(TripsContext)
+
+    const onChanceSetDestino = event => {
+        getTripsByDestino(event.target.value)
+    }
+
+    const getTripsByDestino = (destino) => {
+        axios.get(`http://127.0.0.1:5000/viagem`, {
+            params: {
+                destino
+            }
+        })
+            .then((res) => setTripsConsulta(res.data))
+            .catch(() => alert("Servidor fora do ar!"))
+    }
+
     return (
-        <form action="" method="get" className='formGetTrips'>
-            <input type="text" />
+        <form className='formGetTrips'>
+            <input type="text" name='viagem' onChange={onChanceSetDestino} />
             <input type="submit" value="Pesquisar" />
         </form>
     )
