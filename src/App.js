6b2731bb57bb5792f383f35b44845556d08e09db
caioteixeira <caio.h.teixeira@gmail.com>
2024-06-09 16:12:34 -0300

    Ajuste no import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
